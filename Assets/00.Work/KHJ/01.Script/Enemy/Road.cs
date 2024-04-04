6e431f9f869cbded736de2b07fb589ef4a304270
ujn007 <hyunjun9531@gmail.com>
2024-04-04 18:41:52 +0900

    .

--- Assets/00.Work/KHJ/01.Script/Enemy/Road.cs
+++ Assets/00.Work/KHJ/01.Script/Enemy/Road.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Road : MonoBehaviour
 {
     [field: SerializeField] public List<Road> connectRoad { get; private set; } = new();
-    [SerializeField] private LayerMask roadMask;
+    [SerializeField] private LayerMask tileMask;
+    [SerializeField] private float distance;
     [SerializeField] private float radius;
 
-    private void Awake()
+    private void Start()
     {
         Vector2[] dirs = { Vector2.up, Vector2.down,
                           Vector2.right, Vector2.left};
 
-        //foreach (var dir in dirs)
-        //{
-        //    if (Physics2D.CircleCast(transform.position, radius,  dir, 1.4f, roadMask))
-        //    {
-        //        connectRoad.Add(info.transform.GetComponent<Road>());
-        //    }
-        //}
+        foreach (var dir in dirs)
+        {
+            Collider2D col = Physics2D.OverlapCircle(transform.localPosition * dir, radius, tileMask);
+            print(col.transform.position);
+
+            col.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        }
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Vector2[] dirs = { Vector2.up, Vector2.down,
+                          Vector2.right, Vector2.left};
+
+        foreach (Vector2 dir in dirs)
+            Gizmos.DrawWireSphere(transform.position + (Vector3)dir * distance, radius);
+    }
+#endif 
 }
