6c171ef6b67ec62d26bd11fbfbb3287536608236
ujn007 <hyunjun9531@gmail.com>
2024-05-01 18:13:50 +0900

    EnemyLast

--- Assets/00.Work/KHJ/01.Script/Enemy/EnemyGroup.cs
+++ Assets/00.Work/KHJ/01.Script/Enemy/EnemyGroup.cs
@@ -1,8 +1,46 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public abstract class EnemyGroup : MonoBehaviour 
+public abstract class EnemyGroup : MonoBehaviour
 {
-    
+    [HideInInspector] public List<PlayerPieces> playerPieces = new List<PlayerPieces>();
+    [HideInInspector] public List<Enemy> enemyList = new List<Enemy>();
+
+    public virtual void Start()
+    {
+        playerPieces = FindObjectsOfType<PlayerPieces>().ToList();
+        enemyList = FindObjectsOfType<Enemy>().ToList();
+    }
+
+    public string CheckMinEnemy()
+    {
+        float mindis = float.MaxValue;
+        Enemy enemy = null;
+
+        for (int i = 0; i < playerPieces.Count; i++)
+        {
+            for (int j = 0; j < enemyList.Count; j++)
+            {
+                float dis = Vector2.Distance(playerPieces[i].transform.position, enemyList[j].transform.position);
+
+                if (dis <= mindis)
+                {
+                    if (dis == mindis)
+                    {
+                        
+                    }
+                    mindis = dis;
+                    enemy = enemyList[j];
+                }
+            }
+        }
+
+        return enemy.name;
+    }
+
+    private void CheckLowHealth()
+    {
+
+    }
 }
--- Assets/00.Work/KHJ/01.Script/Enemy/EnemyState/EnemyMoveState.cs
+++ Assets/00.Work/KHJ/01.Script/Enemy/EnemyState/EnemyMoveState.cs
@@ -6,36 +6,33 @@ public class EnemyMoveState : EnemyState
 {
     public bool canMove;
 
+    #region ref
     public Transform moveToTrm = null;
     public GameObject moveToObj = null;
+    #endregion
 
     public EnemyMoveState(Enemy _enemy, EnemyStateMachine _stateMachine) : base(_enemy, _stateMachine)
     {
-    }
-
-    public override void Enter()
-    {
-        List<Transform> goRoads = enemy.targets;
     }
 
     public override void UpdateState()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (!canMove)
+            if (!canMove && enemy.so.name == enemy.CheckMinEnemy())
             {
                 enemy.CheckRoad(ref moveToTrm, ref moveToObj);
+                moveToTrm.position += Vector3.forward;
 
                 Debug.Log(moveToObj.layer);
-                if (moveToObj.layer == 7) //8이 플레이어
+                if (moveToObj.layer == 7) //7이 플레이어
                 {
                     stateMachine.ChangeState(EnemyStateEnum.Attack);
                 }
                 else
                 {
-                    enemy.transform.DOMove(moveToTrm.position, 0.5f).OnComplete(() => canMove = false);
+                    enemy.transform.DOMove(moveToTrm.position + Vector3.forward, 0.5f).OnComplete(() => canMove = false);
                 }
-                canMove = true;
             }
         }
     }
