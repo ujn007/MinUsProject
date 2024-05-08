bafd222f1ddbb6a64d6d0f56e778fbd35b66936e
ujn007 <hyunjun9531@gmail.com>
2024-05-08 18:56:27 +0900

    enemyFixed

--- Assets/00.Work/KHJ/01.Script/Enemy/EnemyState/EnemyAttackState.cs
+++ Assets/00.Work/KHJ/01.Script/Enemy/EnemyState/EnemyAttackState.cs
@@ -1,4 +1,6 @@
 using DG.Tweening;
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class EnemyAttackState : EnemyState
@@ -12,9 +14,7 @@ public class EnemyAttackState : EnemyState
     public override void Enter()
     {
         base.Enter();
-
         moveState = stateMachine.GetState(EnemyStateEnum.Move) as EnemyMoveState;
-        Debug.Log(moveState);
 
         Attack();
     }
@@ -26,16 +26,29 @@ public class EnemyAttackState : EnemyState
         Sequence sq = DOTween.Sequence();
         sq.Append(enemy.MoveTween((Vector2)moveState.moveToTrm.position, 0.1f)).OnComplete(() =>
         {
+            Debug.Log(moveState.moveToObj.name);
             if (moveState.moveToObj.TryGetComponent(out PlayerPieces player))
             {
-                
+                Debug.Log("플레이어 찾아옴");
+                player.HitPiece(1);
+                enemy.StartCoroutine(HitEffectCoroutine(player));
             }
         });
 
         sq.Append(enemy.MoveTween(startPos, 0.5f, Ease.OutQuart)).OnComplete(() =>
         {
-            moveState.canMove = false;  
+            moveState.canMove = false;
             stateMachine.ChangeState(EnemyStateEnum.Move);
         });
+    }
+
+    private IEnumerator HitEffectCoroutine(PlayerPieces player)
+    {
+        Material mat= player.GetComponent<SpriteRenderer>().material;
+        mat.SetColor("_EmissionColor", Color.white);
+         
+        yield return new WaitForSeconds(0.1f);
+
+        mat.SetColor("EmissionColor", Color.black);
     }
 }
